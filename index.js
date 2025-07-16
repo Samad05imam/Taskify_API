6b0327baab10cbd86365bb08f9ecf3a9ccc66cad
Samad05imam <samad1419imam@gmail.com>
2025-07-17 02:06:31 +0530

    Initial commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser())
 const corsOption = {
-    origin : "https://taskify-samad05imam.netlify.app/",
+    origin : ["https://taskify-samad05imam.netlify.app"],
     credentials : true
 }
 app.use(cors(corsOption));
